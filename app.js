afef3b228a9e8cc6870a802c2c7b710a3293cc42
Keagan Dickinson <keagantdickinson@gmail.com>
2024-10-22 22:17:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const mysql = require("mysql");
 
 const app = express();
+
+const db = mysql.createConnection({
+    host: 'phisherftw.com',
+    port: 3306,
+    database: 'phisdvms_APPDB',
+    user: 'phisdvms_admin',
+    password: 'veMQsLzHv2mWZ@A',
+});
+
+db.connect((error) => {
+    if(error) {
+        console.log(error)
+    } else {
+        console.log("MYSQL Connected...")
+    }
+})
 
 app.get("/", (req, res) => {
     res.send("<h1>Home Page</h1>")
